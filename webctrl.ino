6e787bb7edc21ddbf32d3dd69e569664e4fa80e4
YUZHENFEI <zhenfei.yu@utbm.fr>
2019-01-22 18:04:19 +0800

    Update webctrl.ino
    add function for 2 servos

--- webctrl.ino
+++ webctrl.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
 HardwareSerial & esp8266 = Serial2;
-
 #define DEBUG true 
-#define sg90_pin 9 
-
-Servo sg90; 
+#define sg92r_pin 9 
+#define sg92_pin 10 
+Servo sg92r,sg92;
 
 int current_position = 0;
+int current_position1 = 0;
 int vel = 10; 
 int minimum_position = 0; 
 int maximum_position = 180;
@@ -14,9 +14,12 @@ int maximum_position = 180;
 
 void setup()
 {
-sg90.attach(sg90_pin);
-sg90.write(maximum_position);
-sg90.detach();
+sg92r.attach(sg92r_pin);
+sg92r.write(maximum_position);
+sg92r.detach();
+sg92.attach(sg92_pin);
+sg92.write(maximum_position);
+sg92.detach();
 Serial.begin(115200);
 esp8266.begin(115200);
 
@@ -40,8 +43,8 @@ if (esp8266.find("+IPD,"))
 String msg;
 esp8266.find("?"); 
 msg = esp8266.readStringUntil(' '); 
-String command = msg.substring(0, 3); 
-String valueStr = msg.substring(4);   
+String command = msg.substring(0, 3); // intercept first 3 chars
+String valueStr = msg.substring(4);   //intercept from the 5th char
 int value = valueStr.toInt();         
 if (DEBUG) {
 Serial.println(command);
@@ -50,7 +53,7 @@ Serial.println(value);
 delay(100);
 
 
-//move servo1 to desired angle
+//move servo1 to desired angle in vertical direction
 if(command == "sr1") {
 //limit input angle
 if (value >= maximum_position) {
@@ -59,20 +62,45 @@ value = maximum_position;
 if (value <= minimum_position) {
 value = minimum_position;
 }
-sg90.attach(sg90_pin); //attach servo
+sg92r.attach(sg92r_pin); //attach servo
 while(current_position != value) {
 if (current_position > value) {
 current_position -= 1;
-sg90.write(current_position);
+sg92r.write(current_position);
 delay(100/vel);
 }
 if (current_position < value) {
 current_position += 1;
-sg90.write(current_position);
+sg92r.write(current_position);
 delay(100/vel);
 }
 }
-sg90.detach(); //dettach
+sg92r.detach(); //dettach
+}
+
+//move servo1 to desired angle in horizontal direction
+if(command == "sr2") {
+//limit input angle
+if (value >= maximum_position) {
+value = maximum_position;
+}
+if (value <= minimum_position) {
+value = minimum_position;
+}
+sg92.attach(sg92_pin); //attach servo
+while(current_position1 != value) {
+if (current_position1 > value) {
+current_position1 -= 1;
+sg92.write(current_position1);
+delay(100/vel);
+}
+if (current_position1 < value) {
+current_position1 += 1;
+sg92.write(current_position1);
+delay(100/vel);
+}
+}
+sg92.detach(); //dettach
 }
 
 
